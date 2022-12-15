03b5d89c67fc95177830985365659f84b85c7637
LucasMiller98 <lucas.miller1998@outlook.com>
2022-12-15 01:41:28 -0300

    Song Play

---
+++ mobile-happy/App.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -8.1256899,
+          longitude: -35.0278015,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
+});
